c23b3b29f476986943e188a2960789ab39f7f597
Igor Fonseca <igor.stutzf@gmail.com>
2022-05-03 11:17:33 -0300

    update conheça-benefícios

--- template-parts/sections/home/conheca-beneficios.php
+++ template-parts/sections/home/conheca-beneficios.php
@@ -10,13 +10,11 @@
               <h4>Saúde Mental</h4>
               <span>+</span>
           </div>
-           <div class="hover">
+           <div class="hover mt-3">
             <p>
               Aumenta o foco, a criatividade e a clareza mental. Melhora a memória, a percepção e a capacidade de analisar e resolver problemas.
             </p>
-            <a href="#" class="btn">
-              + Saiba mais
-            </a>
+            
           </div>
         </div>
       </div>
@@ -28,13 +26,11 @@
             <h4>Saúde Física</h4>
             <span>+</span>
           </div>
-           <div class="hover">
+           <div class="hover mt-3">
             <p>
               Promove autoconhecimento, empatia, melhora a qualidade dos relacionamentos e leva à autorrealização.
             </p>
-            <a href="#" class="btn">
-              + Saiba mais
-            </a>
+            
           </div>
         </div>
       </div>
@@ -46,13 +42,11 @@
             <h4>Inteligência emocional</h4>
             <span>+</span>
           </div>
-          <div class="hover">
+          <div class="hover mt-3">
             <p>
               Melhora a qualidade do sono, a frequência cardíaca, a pressão arterial e outros importantes indicadores de saúde.
             </p>
-            <a href="#" class="btn">
-              + Saiba mais
-            </a>
+            
           </div>
         </div>
       </div>
@@ -64,13 +58,11 @@
             <h4>Potencial Mental</h4>
             <span>+</span>
             </div>
-             <div class="hover">
+             <div class="hover mt-3">
             <p>
               Reduz ansiedade, depressão, síndrome do pânico e  burnout. Promove paz interior, autoconfiança e felicidade.
             </p>
-            <a href="#" class="btn">
-              + Saiba mais
-            </a>
+            
           </div>
         </div>
       </div>
