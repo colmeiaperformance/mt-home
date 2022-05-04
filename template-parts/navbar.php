c4e37718c0513c69aa7ddb6aa1f048e069ce5c27
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-05-04 18:06:03 -0300

    Update navbar

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -3,10 +3,7 @@
     <a class="navbar-brand" href="<?php home_url(); ?>">
        <img src=<?php echo get_template_directory_uri() . '/images/header/logo-topo.png' ?> alt="Logotipo Meditação Transcedental" height="auto" width="256px">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-      aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <img src=<?php echo get_template_directory_uri() . '/images/header/toggler.png' ?> alt="Menu">
-    </button>
+    
     <div class="collapse navbar-collapse ms-auto" id="navbarCollapse">
       <div class="d-flex align-items-center my-3 m-lg-0">
       </div>
