7bcea19d5453540bd02737ce6665b8594cc8035f
Igor Fonseca <igor.stutzf@gmail.com>
2022-05-10 16:27:40 -0300

    update agende-palestra, como-aprender

--- template-parts/sections/home/agende-palestra.php
+++ template-parts/sections/home/agende-palestra.php
@@ -4,7 +4,9 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle green rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+600</h4>
+            <div id="contagem-1">
+              <h4 id="numero-1">+600</h4>
+            </div>
             <p>estudos científicos</p>
           </div>
         </div>
@@ -12,7 +14,9 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle red rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+10</h4>
+            <div id="contagem-2">
+              <h4 id="numero-2">+10</h4>
+            </div>
             <p>milhões de meditantes</p>
           </div>
         </div>
@@ -20,7 +24,9 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle blue rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+100</h4>
+            <div id="contagem-3">
+              <h4 id="numero-3">+100</h4>
+            </div>
             <p>países</p>
           </div>
         </div>
@@ -28,7 +34,9 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle yellow rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+500</h4>
+            <div id="contagem-4">
+              <h4 id="numero-4">+500</h4>
+            </div>
             <p>mil meditantes no Brasil</p>
           </div>
         </div>
@@ -40,6 +48,134 @@
         <a class="btn" href="https://meditacaotranscendental.com.br/mt-onde-voce-esta-2022/" role="button">Agende a palestra gratuita</a>
       </div>
     </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script>
+
+      // jQuery('#contagem-1').on('mouseover', function() {
+      //   var numero = document.getElementById('numero-1');
+      //   var min = 1;
+      //   var max = 600;
+      //   var duração = max*15; 
+
+      //   for (var i = min; i <= max; i++) {
+      //     setTimeout(function(nr) {
+      //       numero.innerHTML = '+'+nr;
+      //     }, i * (max*15) / max, i);
+      //   }
+      // });
+
+      // jQuery('#contagem-2').on('mouseover', function() {
+      //   var numero = document.getElementById('numero-2');
+      //   var min = 1;
+      //   var max = 10;
+      //   var duração = max*150;
+
+      //   for (var i = min; i <= max; i++) {
+      //     setTimeout(function(nr) {
+      //       numero.innerHTML = '+'+nr;
+      //     }, i * (max*150) / max, i);
+      //   }
+      // });
+
+      // jQuery('#contagem-3').on('mouseover', function() {
+      //   var numero = document.getElementById('numero-3');
+      //   var min = 1;
+      //   var max = 100;
+      //   var duração = max*30;
+
+      //   for (var i = min; i <= max; i++) {
+      //     setTimeout(function(nr) {
+      //       numero.innerHTML = '+'+nr;
+      //     }, i * (max*30) / max, i);
+      //   }
+      // });
+        
+      // jQuery('#contagem-4').show(function() {
+      //   var numero = document.getElementById('numero-4');
+      //   var min = 1;
+      //   var max = 500;
+      //   var duração = max*1
+      //   for (var i = min; i <= max; i++) {
+      //     setTimeout(function(nr) {
+      //       numero.innerHTML = '+'+nr;
+      //     }, i * (max*15) / max, i);
+      //   }
+      // });
+
+
+      // jQuery(document).ready(checkContainer);
+
+      // function checkContainer () {
+      //   if($('.agende-palestra').is(':visible')){ //if the container is visible on the page
+      //     createGrid();  //Adds a grid to the html
+      //   } else {
+      //     setTimeout(checkContainer, 50); //wait 50 ms, then try again
+      //   }
+      // }
+      $myDiv = $(".agende-palestra");
+
+      $myDiv.hide();
+
+
+      // $('.agende-palestra').one('ready', function(){
+      //     alert('OLÁ');
+      // });
+
+
+
+      $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 70) {
+          $myDiv.show(function() { 
+              var numero = document.getElementById('numero-1');
+              var min = 1;
+              var max = 600;
+              // var duração = max*1
+              for (var i = min; i <= max; i++) {
+                setTimeout(function(nr) {
+                  numero.innerHTML = '+'+nr;
+                }, i * (max*15) / max, i);
+              }
+          });
+          $myDiv.show(function() { 
+              var numero = document.getElementById('numero-2');
+              var min = 1;
+              var max = 10;
+              // var duração = max*1
+              for (var i = min; i <= max; i++) {
+                setTimeout(function(nr) {
+                  numero.innerHTML = '+'+nr;
+                }, i * (max*400) / max, i);
+              }
+          });
+          $myDiv.show(function() { 
+              var numero = document.getElementById('numero-3');
+              var min = 1;
+              var max = 100;
+              // var duração = max*1
+              for (var i = min; i <= max; i++) {
+                setTimeout(function(nr) {
+                  numero.innerHTML = '+'+nr;
+                }, i * (max*100) / max, i);
+              }
+          });
+          $myDiv.show(function() { 
+              var numero = document.getElementById('numero-4');
+              var min = 1;
+              var max = 500;
+              // var duração = max*1
+              for (var i = min; i <= max; i++) {
+                setTimeout(function(nr) {
+                  numero.innerHTML = '+'+nr;
+                }, i * (max*20) / max, i);
+              }
+          });
+        }
+      });
+
+
+    </script>
 
   </div>
 </section>
--- template-parts/sections/home/como-aprender.php
+++ template-parts/sections/home/como-aprender.php
@@ -3,7 +3,7 @@
     <h2 class="como-aprender-title">Como aprender <strong><span  class="yellow">MT</span></strong> ?</h2>
     <div class="row align-items-center justify-content-center">
 
-      <div class="col-12 col-lg-6 d-block d-lg-flex">
+      <div class="col-12 col-lg-6 d-none d-lg-block d-lg-flex">
         <div class="col-12 col-lg-4 d-flex align-items-start justify-content-center">
           <img class="img-fluid como-aprender-img" src=<?php echo get_template_directory_uri() . '/images/aprenda-mt/icon-arvore.png' ?>
             alt="Aprenda MT">
@@ -15,12 +15,17 @@
         </div>
       </div>
 
+      <div class="row text-call mt-0 mt-lg-4 d-block d-lg-none text-center">
+        <p>
+          O <strong>primeiro passo</strong> para aprender é participar de uma <strong><span class="yellow">palestra online gratuita</span></strong>.
+        </p>
+      </div>
+
       <div class="col-12 col-lg-6">
         <div class="como-aprender-box">
           <ul class="como-aprender-list">
+            <li class="como-aprender-list-item">A Meditação Transcendental é ensinada apenas por instrutores certificados e credenciados</li>
             <li class="como-aprender-list-item">O curso é realizado em 4 encontros presenciais.</li>
-            <li class="como-aprender-list-item">O primeiro dia é individual e os demais podem ser em pequenos grupos.
-            </li>
             <li class="como-aprender-list-item">Você aprende a meditar através da experiência prática com a MT.</li>
             <li class="como-aprender-list-item">Após o curso, você terá acompanhamento sempre que precisar.</li>
           </ul>
@@ -30,13 +35,13 @@
 
     </div>
 
-    <div class="row text-call mt-sm-4 text-center">
+    <div class="row text-call mt-sm-4 d-none d-lg-block text-center">
       <p>
         O <strong>primeiro passo</strong> para aprender é participar de uma <strong><span class="yellow">palestra online gratuita</span></strong>.
       </p>
     </div>
 
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-4 mt-lg-0">
       <div class="col-12 text-center">
         <a class="btn px-4" href="https://meditacaotranscendental.com.br/mt-onde-voce-esta-2022/" role="button">Inscreva-se</a>
       </div>
