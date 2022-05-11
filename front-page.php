cf6dde0679ab4b7c02c83338c5cc7e27abcfdfed
Igor Fonseca <igor.stutzf@gmail.com>
2022-05-11 17:57:46 -0300

    update visite-blog, update agende-palestra

--- front-page.php
+++ front-page.php
@@ -10,6 +10,7 @@
   <?php get_template_part('template-parts/sections/home/o-que-dizem'); ?>
   <?php get_template_part('template-parts/sections/home/transcender'); ?>
   <?php get_template_part('template-parts/sections/home/mundo-melhor'); ?>
+  <?php get_template_part('template-parts/sections/home/visite-blog'); ?>
   <?php get_template_part('template-parts/sections/home/agende-palestra'); ?>
 
 </main>
