084a9d45fce5e4aac47c004f4c4de29992331e5b
Igor Fonseca <igor.stutzf@gmail.com>
2022-05-12 17:24:29 -0300

    Revert "update visite-blog, update agende-palestra"
    This reverts commit cf6dde0679ab4b7c02c83338c5cc7e27abcfdfed.

--- template-parts/sections/home/visite-blog.php
+++ template-parts/sections/home/visite-blog.php
@@ -1,95 +1,143 @@
 <section class="visite-blog">
-    <div class="container py-5">
-      <h2>Visite o nosso <strong>blog</strong></h2>
-      <div id="oquedizem" class="swiper swiperConhecaBlog" data-bs-ride="carousel">
+  <div class="container py-5">
+    <h2>Visite o nosso <strong>blog</strong></h2>
+    <div class="row align-items-center justify-content-center">
+
+      <div id="visite-blog-carousel" class="swiper" data-bs-ride="carousel">
         <div class="swiper-wrapper">
 
-          <div class="swiper-slide col-12 col-lg-6">
-            <div class="p-3 col d-flex">
-              <div class="post p-3 col-lg-12 d-lg-flex">
-                <div class="post-img col mb-2 mb-lg-0 col-lg-5">
+          <div class="swiper-slide text-center">
+            <div class="row">
+              <div class="col-6 d-lg-flex">
+                <div class="col">
                   <img class="img-fluid" src="https://picsum.photos/800/500?random=1" alt="">
                 </div>
-                <div class="col post-item d-flex flex-column text-center text-lg-start">
-                  <div class="item-content">
-                    <p>
-                      <strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
-                    <p>
-                      Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
-                    </p>
-                  </div>
-                  <div class="item-btn d-flex justify-content-center justify-content-lg-end">
-                    <a class="btn visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
-                  </div>
+                <div class="col visite-blog-item d-block text-center text-lg-start">
+                  <p><strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                  <p>Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                  </p>
+                  <a class="btn btn-sm visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
+                </div>
+              </div>
+              <div class="col-6 d-lg-flex">
+                <div class="col">
+                  <img class="img-fluid" src="https://picsum.photos/800/500?random=2" alt="">
+                </div>
+                <div class="col visite-blog-item d-block text-center text-lg-start">
+                  <p><strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                  <p>Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                  </p>
+                  <a class="btn btn-sm visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="swiper-slide col-12 col-lg-6">
-            <div class="p-3 col d-flex">
-              <div class="post p-3 col-lg-12 d-lg-flex">
-                <div class="post-img col mb-2 mb-lg-0 col-lg-5">
-                  <img class="img-fluid" src="https://picsum.photos/800/500?random=1" alt="">
+          <div class="swiper-slide text-center">
+            <div class="row">
+              <div class="col-6 d-lg-flex">
+                <div class="col">
+                  <img class="img-fluid" src="https://picsum.photos/800/500?random=3" alt="">
                 </div>
-                <div class="col post-item d-flex flex-column text-center text-lg-start">
-                  <div class="item-content">
-                    <p>
-                      <strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
-                    <p>
-                      Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
-                    </p>
-                  </div>
-                  <div class="item-btn d-flex justify-content-center justify-content-lg-end">
-                    <a class="btn visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
-                  </div>
+                <div class="col visite-blog-item d-block text-center text-lg-start">
+                  <p><strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                  <p>Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                  </p>
+                  <a class="btn btn-sm visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
+                </div>
+              </div>
+              <div class="col-6 d-lg-flex">
+                <div class="col">
+                  <img class="img-fluid" src="https://picsum.photos/800/500?random=4" alt="">
+                </div>
+                <div class="col visite-blog-item d-block text-center text-lg-start">
+                  <p><strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                  <p>Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                  </p>
+                  <a class="btn btn-sm visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
                 </div>
               </div>
             </div>
           </div>
 
-          
+          <div class="swiper-slide text-center">
+            <div class="row">
+              <div class="col-6 d-lg-flex">
+                <div class="col">
+                  <img class="img-fluid" src="https://picsum.photos/800/500?random=5" alt="">
+                </div>
+                <div class="col visite-blog-item d-block text-center text-lg-start">
+                  <p><strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                  <p>Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                  </p>
+                  <a class="btn btn-sm visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
+                </div>
+              </div>
+              <div class="col-6 d-lg-flex">
+                <div class="col">
+                  <img class="img-fluid" src="https://picsum.photos/800/500?random=6" alt="">
+                </div>
+                <div class="col visite-blog-item d-block text-center text-lg-start">
+                  <p><strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                  <p>Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                  </p>
+                  <a class="btn btn-sm visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button class="swiper-button-prev" type="button" data-bs-target="#visite-blog-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="swiper-button-next" type="button" data-bs-target="#visite-blog-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+          <div class="swiper-scrollbar"></div>
+          <!-- <div class="carousel-indicators">
+            <button type="button" data-bs-target="#visite-blog-carousel" data-bs-slide-to="0" class=""
+              aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#visite-blog-carousel" data-bs-slide-to="1" aria-label="Slide 2"
+              class="active" aria-current="true"></button>
+            <button type="button" data-bs-target="#visite-blog-carousel" data-bs-slide-to="2" aria-label="Slide 3"
+              class="active" aria-current="true"></button>
+          </div> -->
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-pagination"></div>
+
       </div>
     </div>
 </section>
 <script>
-  jQuery(document).ready(function() {
-    var swiper = new Swiper(".swiperConhecaBlog", {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      loop: true,
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      mousewheel: true,
-      keyboard: true,
-      breakpoints: {
-        640: {
-          slidesPerView: 1,
-          slidesPerGroup: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-          spaceBetween: 20,
-        },
-      },
-    });
+jQuery(document).ready(function() {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
-  </script>
+
+});
+// jQuery(document).ready(function() {
+//   jQuery('#visiteBlogCarousel.slide').carousel({
+//     interval: 5000,
+//     pause: "hover"
+//   });
+// });
+</script>
